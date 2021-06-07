082bf928a097a3a9afba625c20786e7ef2664039
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-06-07 17:09:23 +0530

    add support for multiple files

--- js/index.js
+++ js/index.js
@@ -6,39 +6,40 @@ const wasm = import("../pkg/index").then((wasm) => {
 			console.info("Starting processing... Calling into Rust module.");
 	
 			let result = wasm.run_json_exported(args);
+			let info_string = `Processed ${result.get_processed_num()} reads.\nJSON output:`;
+			let output_string = result.get_output();
+
+			// creates result_entry onto which both items are appended
+			var result_entry = document.createElement('li');
+			result_entry.appendChild(document.createTextNode(info_string));
+			let output_node = document.createElement('pre');
+			output_node.innerText = output_string;
+
+			// then result_entry is appended into output list
+			result_entry.appendChild(output_node);
 	
-			let result_para = document.getElementById('result_para');
-			result_para.innerHTML = `Processed ${result.get_processed_num()} reads.\nJSON output:`;
-	
-			let output_para = document.getElementById('output');
-			output_para.innerHTML = result.get_output();
+			document.getElementById('output-list').appendChild(result_entry);
 		};
 		fileReader.readAsText(file);
-	
-		let result_para = document.getElementById('result_para');
-		result_para.innerHTML = `Processing...`;
 	}
 	
 	function run() {
 		const fileSelector = document.getElementById('file-selector');
 	
-		const file = fileSelector.files[0];
-		console.info(file);
-	
-		// remember: current input mechanism relies on modifying elements
-		// change that if you want to have multiple files work
-		// and also add "multiple" attribute to the HTML input
-	
-		console.info("Loading WASM");
-		processFile (
-			file,
-			new wasm.SampleArgs (
-				BigInt (100),
-				0,
-				null,
-				50
-			),
-		);
+		for ( var i = 0; i < fileSelector.files.length; i++) {
+			let file = fileSelector.files[i];
+			console.info(file);
+		
+			processFile (
+				file,
+				new wasm.SampleArgs (
+					BigInt (100),
+					0,
+					null,
+					50
+				),
+			);
+		}
 	}
 	
 	document.getElementById('file-selector').addEventListener('change', run);
