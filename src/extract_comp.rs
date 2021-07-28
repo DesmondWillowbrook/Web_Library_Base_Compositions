caf6ca4274c26acfa802327c6ceb78aefdac6f2e
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-07-28 15:32:03 +0530

    use Libarary_Base_Composition - fastq2comp library as dependency instead of copy-pasting code 🤦‍♂️

--- src/extract_comp.rs
+++ src/extract_comp.rs
@@ -1,72 +1,5 @@
-use std::io::BufRead;
-use crate::BaseComp;
+use fastq2comp::extract_comp::{FASTQReader, SampleArgs as InternalSampleArgs, run_json, run_tsv};
 
-#[cfg(test)]
-mod sample_fastq_tests {
-    use super::*;
-    use crate::test_utils::*;
-
-    #[test]
-    fn test_json_run() {
-        let reader = return_reader(b"@\nAAA\n+\n~~~");
-        let args = SampleArgs {
-            target_read_count: 1u64,
-            min_phred_score: 0,
-            n_content: None,
-            trimmed_length: Some(2)
-        };
-
-        let (result, seqs) = run_json( FASTQReader::new(args, reader));
-
-        assert_eq!(
-            result,
-            std::str::from_utf8(b"{\"lib\":[{\"pos\":1,\"bases\":{\"A\":100,\"T\":0,\"G\":0,\"C\":0,\"N\":0}},{\"pos\":2,\"bases\":{\"A\":100,\"T\":0,\"G\":0,\"C\":0,\"N\":0}}],\"len\":2}").unwrap()
-        );
-        assert_eq!(seqs, 1);
-    }
-
-    #[test]
-    fn test_csv_run() {
-        let reader = return_reader(b"@\nAAA\n+\n~~~");
-        let args = SampleArgs {
-            target_read_count: 1u64,
-            min_phred_score: 0,
-            n_content: None,
-            trimmed_length: Some(2)
-        };
-
-        let (result, seqs) = run_csv( FASTQReader::new(args, reader));
-
-        assert_eq!(
-            result,
-            std::str::from_utf8(b"100,0,0,0,0,100,0,0,0,0").unwrap()
-        );
-        assert_eq!(seqs, 1);
-    }
-
-    #[test]
-    fn test_check_colorspace() {
-        let mut read = FASTQRead::new(6);
-        let mut reader = return_reader(b"@\nAT1CGN\n+\n!!!!!!");
-        read.read(&mut reader, None);
-
-        assert!(read.check_colorspace("AT1CGN"))
-    }
-
-    #[test]
-    fn test_count_n() {
-        let mut read = FASTQRead::new(6);
-        let mut reader = return_reader(b"@\n\n+\n!!!!!!");
-        read.read(&mut reader, None);
-
-        assert_eq!(FASTQRead::count_n("NNANNA"), 4)
-    }
-
-    #[test]
-    fn test_get_average_quality() {
-        assert_eq!(FASTQRead::get_average_quality("#{|}"), 68)
-    }
-}
 use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen]
 #[derive(Debug)]
@@ -82,132 +15,26 @@ pub struct SampleArgs {
     pub trimmed_length: Option<usize>,
 }
 
+impl From<SampleArgs> for InternalSampleArgs {
+    fn from(s: SampleArgs) -> Self {
+        InternalSampleArgs {
+            target_read_count: s.target_read_count,
+            min_phred_score: s.min_phred_score,
+            n_content: s.n_content,
+            trimmed_length: s.trimmed_length,
+        }
+    }
+}
+
 #[wasm_bindgen]
 impl SampleArgs {
     #[wasm_bindgen (constructor)]
     pub fn new(target_read_count: u64, min_phred_score: usize, n_content: Option<usize>, trimmed_length: Option<usize>) -> Self { Self { target_read_count, min_phred_score, n_content, trimmed_length } }
 }
 
-use regex::Regex;
-
-use lazy_static::lazy_static;
-lazy_static! {
-    // Regex for checking if seq has numbers
-    static ref SEQCOLORSPACECHECKER: Regex = Regex::new(r"\d").unwrap();
-}
-
-/// Abstraction for a single read of FASTQ data
-#[derive(Debug)]
-pub(crate) struct FASTQRead {
-    pub seq: String,
-    quals: String,
-}
-
-use crate::io_utils::get_reader;
-use log::{info, debug};
-
-impl FASTQRead {
-
-    /// Reads a complete FASTQ statement (composed of 4 lines) into itself
-    /// - `reader`: Object implementing `std::io::BufRead` from which to read lines
-    /// - Returns `None` if EOF reached.
-    fn read_fastq (&mut self, reader: &mut impl BufRead) -> Option<()> {
-        //Skips the 1st and 3rd line resp. in 4 lines of input
-        for s in [&mut self.seq, &mut self.quals].iter_mut() {
-            **s = match reader.lines().nth(1) {
-                Some(n) => n.expect("Error reading line. Make sure UTF-8 input is supported"),
-                None => {info!("Input reading finished"); return None},
-            }
-        }
-
-        Some(())
-    }
-
-    /// Reads a complete FASTQ statement (composed of 4 lines) into itself.
-    /// - *Important*: Panics if read length of strings is not same as strings read before.
-    /// - `reader`: Object implementing `std::io::BufRead` from which to read lines
-    /// - Returns `None` if EOF reached.
-    fn read (&mut self, reader: &mut impl BufRead, len: Option<usize>) -> Option<()>
-    {
-        let (seq_len, quals_len) = (self.seq.len(), self.quals.len());
-        
-        let result = self.read_fastq(reader);
-
-        // If there have been no reads, then perform first read without checking for lengths
-        if seq_len == 0 || quals_len == 0 {
-            return result;
-        }
-
-        // behaviour is meant to panic only when uneven reads cannot be fixed by trimming
-        if len.unwrap_or(usize::MAX) > seq_len && (seq_len != self.seq.len() || quals_len != self.quals.len()) {
-            panic!("Reads have inconsistent lengths. Particularly this read: \n{}\n{}\n \
-            Expected read to be: {} length but it was: {} length", self.seq, self.quals, seq_len, self.seq.len());
-        }
-
-        result
-    }
-
-    fn new (len: usize) -> FASTQRead {
-        FASTQRead {
-            seq: String::with_capacity(len),
-            quals: String::with_capacity(len),
-        }
-    }
-
-    fn count_n(seq: &str) -> usize {
-        seq.matches('N').count()
-    }
-
-    // Returns true if number is found in seq
-    fn check_colorspace(&self, seq: &str) -> bool {
-        SEQCOLORSPACECHECKER.is_match(seq)
-    }
-
-    fn get_average_quality(quals: &str) -> usize {
-        let mut qual_sum: usize = 0;
-        for char in quals.as_bytes() {
-            qual_sum += (*char as usize) - 33;
-        }
-
-        qual_sum / quals.len()
-    }
-
-    fn trim(str: &str, len: Option<usize>) -> &str {
-        match len {
-            Some(n) => if n < str.len() {&str[0..n]} else {str},
-            None => str,
-        }
-    }
-
-    fn check_read(&mut self, args: &SampleArgs) -> bool {
-        let seq = FASTQRead::trim(&self.seq, args.trimmed_length);
-        let quals = FASTQRead::trim(&self.quals, args.trimmed_length);
-
-        // Check for numbers in reads
-        if self.check_colorspace(seq) {
-            info!("Found numbers in reads - this is probably colorspace");
-            std::process::exit(0);
-        }
-
-        // Count the N's
-        if let Some(n) = args.n_content {
-            if FASTQRead::count_n(seq) > n {
-                debug!("N count of current line ({}) too high - skipping", FASTQRead::count_n(seq));
-                return false;
-            }
-        }
-
-        if FASTQRead::get_average_quality(quals) < args.min_phred_score {
-            debug!("Quality too low - skipping");
-            return false;
-        }
-
-        true
-    }
-}
+use crate::{io_utils::get_reader, utils::set_panic_hook};
 
 #[wasm_bindgen]
-#[allow(dead_code)]
 pub struct Output {
     processed_num: u64,
     out: String,
@@ -221,116 +48,22 @@ impl Output {
     pub fn get_processed_num (&self) -> u64 {self.processed_num} 
 }
 
+// Entry points here
 #[wasm_bindgen]
 pub fn run_json_exported (sample_args: SampleArgs, compressed: bool) -> Output {
-    let fastq_reader = FASTQReader::new(sample_args, get_reader(compressed));
+    set_panic_hook();
+
+    let fastq_reader = FASTQReader::new(sample_args.into(), get_reader(compressed));
     let (out, processed_num) = run_json (fastq_reader);
     Output {processed_num, out}
 }
 
 #[wasm_bindgen]
-pub fn run_csv_exported (sample_args: SampleArgs, compressed: bool) -> Output {
-    let fastq_reader = FASTQReader::new(sample_args, get_reader(compressed));
-    let (out, processed_num) = run_csv (fastq_reader);
+pub fn run_tsv_exported (sample_args: SampleArgs, compressed: bool) -> Output {
+    set_panic_hook();
+
+    let fastq_reader = FASTQReader::new(sample_args.into(), get_reader(compressed));
+    let (out, processed_num) = run_tsv (fastq_reader);
     Output {processed_num, out}
 }
 
-/// Takes in reader (for FASTQ lines) and SampleArgs, returns JSONified string and
-/// total number of reads processed after applying SampleArgs.
-pub fn run_json<T> (fastq_reader: FASTQReader<T>) -> (String, u64)
-where T: BufRead
-{
-    let (mut comp, lines_read) = run_core (fastq_reader);
-
-    (comp.jsonify(), lines_read)
-}
-
-pub fn run_csv<T> (fastq_reader: FASTQReader<T>) -> (String, u64)
-where T: BufRead
-{
-    let (comp, lines_read) = run_core (fastq_reader);
-
-    ({let mut s = comp.lib.into_iter().flat_map(|b| b.bases.iter()).
-        fold(String::new(), |acc, curr| acc + &curr.to_string() + ",");
-        s.pop(); // remove trailing ',' to make it valid CSV
-        s
-    },
-    lines_read)
-}
-
-/// Takes in reader (for FASTQ lines) and SampleArgs, returns [`BaseComp`] and
-/// total number of reads processed after applying SampleArgs.
-use reservoir_sampling::unweighted::l as sample;
-fn run_core<T> (fastq_reader: FASTQReader<T>) -> (BaseComp, u64)
-where T: BufRead
-{
-    // best place to init error handling, this code path is always taken
-    crate::utils::set_panic_hook();
-    
-    //TODO: Convert args.target_read_count to usize or figure out how to allocate u64-sized vec
-    let mut sampled_seqs = vec![String::new(); fastq_reader.target_read_count as usize];
-    sample(fastq_reader, &mut sampled_seqs);
-
-    // Figure out allotment size based on line size, or provided trim len
-    let mut base_comp = BaseComp::init(sampled_seqs[0].len());
-
-    let mut lines_read = 0u64;
-    for seq in sampled_seqs {
-        if seq.is_empty() {
-            break;
-        }
-        base_comp.extract(&seq);
-        lines_read += 1;
-    }
-
-    for r in base_comp.lib.iter_mut() {
-        r.bases.percentage();
-    }
-
-    (base_comp, lines_read)
-}
-
-
-pub struct FASTQReader<T: BufRead> {
-    curr: FASTQRead,
-    reader: T,
-    sample_args: SampleArgs,
-    pub curr_valid_reads: u64,
-    pub target_read_count: u64,
-}
-
-impl<T: BufRead> FASTQReader<T> {
-    pub fn new (args: SampleArgs, reader: T) -> FASTQReader<T> {
-        let read = FASTQRead::new(args.trimmed_length.unwrap_or(0));
-        let target_read_count = args.target_read_count.clone();
-
-        FASTQReader {
-            curr: read,
-            reader,
-            sample_args: args,
-            curr_valid_reads: 0,
-            target_read_count,
-        }
-    }
-}
-
-impl<T: BufRead> Iterator for FASTQReader<T> {
-    type Item = String;
-
-    fn next (&mut self) -> Option<String> {
-        if self.curr_valid_reads >= self.sample_args.target_read_count {
-            return None
-        }
-
-        loop {
-            if self.curr.read(&mut self.reader, self.sample_args.trimmed_length).is_none() {
-                return None;
-            }
-            if FASTQRead::check_read(&mut self.curr, &self.sample_args) {break;}
-        }
-        self.curr_valid_reads += 1;
-
-        Some(FASTQRead::trim(&self.curr.seq, self.sample_args.trimmed_length).to_string())
-    }
-
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused_unsafe)]
+
 mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -8,28 +10,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub mod extract_comp;
 
-pub mod test_utils {
-    use std::io::Cursor;
-    /// Returns reader which implements Read trait.
-    /// - `s`: Data which should be yielded by the reader upon read
-    pub fn return_reader (s: &[u8]) -> Cursor<&[u8]> {
-        Cursor::new(s)
-    }
-
-    /// Returns a writer which implements Write trait.
-    /// `writer.get_ref()[0..].to_vec()` can be used to get the data written to the writer.
-    pub fn return_writer() -> Cursor<Vec<u8>> {
-        Cursor::new(Vec::<u8>::new())
-    }
-
-    pub fn get_writer_content(writer: Cursor<Vec<u8>>) -> String {
-        std::str::from_utf8(&writer.get_ref()[0..]).unwrap().to_string()
-    }
-}
-
-pub mod io_utils {
-    #[allow(unused_unsafe)]
-    
+pub mod io_utils {    
     use js_sys::Uint8Array;
     use wasm_bindgen::prelude::*;
     use gloo::file::File;
@@ -53,7 +34,7 @@ pub mod io_utils {
     // See: https://github.com/rustwasm/wasm-bindgen/issues/1079#issuecomment-508577627
     #[derive(Debug)]
     pub struct WasmMemBuffer {pos: u64, file: File}
-
+    
     impl WasmMemBuffer {
         pub fn new() -> WasmMemBuffer {
             WasmMemBuffer {file: File::from(unsafe {get_file()}), pos: 0}
@@ -83,220 +64,5 @@ pub mod io_utils {
                 Box::new(GzDecoder::new(WasmMemBuffer::new()))
             } else {Box::new(WasmMemBuffer::new()) as Box<dyn Read>}
         ))
-    }
-}
-
-use serde::{Serialize, Deserialize};
-
-/// Represents a column of base composition data.
-/// Contains base composition along with position information.
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-#[allow(non_snake_case)]
-pub(crate) struct BaseCompCol {
-    pub pos: usize,
-    pub bases: BaseCompColBases,
-}
-
-/// Represents a column of base composition.
-#[derive(Serialize, Deserialize, PartialEq, Debug, Copy, Clone)]
-#[allow(non_snake_case)]
-pub(crate) struct BaseCompColBases {
-    pub A: usize,
-    pub T: usize,
-    pub G: usize,
-    pub C: usize,
-    pub N: usize,
-}
-
-/// Convenience struct for iterating over base composition
-pub(crate) struct IterableBaseCompColBases {
-    bases: BaseCompColBases,
-    state: BaseCompColBasesIteratorState,
-}
-
-impl Iterator for IterableBaseCompColBases {
-    type Item = usize;
-
-    // VERY VERY IMP. If you change this, then also change the impl FromIterator<usize> for BaseCompColBases
-    fn next(&mut self) -> Option<usize> {
-        match self.state {
-            BaseCompColBasesIteratorState::A => {self.state = BaseCompColBasesIteratorState::C; Some(self.bases.A)},
-            BaseCompColBasesIteratorState::C => {self.state = BaseCompColBasesIteratorState::G; Some(self.bases.C)},
-            BaseCompColBasesIteratorState::G => {self.state = BaseCompColBasesIteratorState::T; Some(self.bases.G)},
-            BaseCompColBasesIteratorState::T => {self.state = BaseCompColBasesIteratorState::N; Some(self.bases.T)},
-            BaseCompColBasesIteratorState::N => {self.state = BaseCompColBasesIteratorState::End; Some(self.bases.N)},
-            _ => None
-        }
-    }
-}
-
-impl From<BaseCompColBases> for IterableBaseCompColBases {
-    fn from(base_comp_col_bases: BaseCompColBases) -> IterableBaseCompColBases {
-        IterableBaseCompColBases {
-            bases: base_comp_col_bases,
-            state: BaseCompColBasesIteratorState::A
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-pub(crate) enum BaseCompColBasesIteratorState {
-    A,
-    C,
-    G,
-    T,
-    N,
-    End,
-}
-
-impl BaseCompColBases {
-    pub fn iter(self) -> impl Iterator<Item = usize> {
-        IntoIterator::into_iter(IterableBaseCompColBases::from(self))
-    }
-
-    pub fn new() -> BaseCompColBases {
-        BaseCompColBases {A: 0, G: 0, T: 0, C: 0, N: 0}
-    }
-
-    pub fn percentage (&mut self) {
-        let sum = self.iter().sum::<usize>() as f64;
-        *self = self.iter().map(|base| ((base as f64 / sum)  * 100f64).round() as usize).collect();
-    }
-}
-
-use std::iter::FromIterator;
-impl FromIterator<usize> for BaseCompColBases {
-    fn from_iter<I: IntoIterator<Item=usize>>(iter: I) -> Self {
-        let mut c = IterableBaseCompColBases::from(BaseCompColBases {A: 0, G: 0, C: 0, T: 0, N: 0});
-        let mut iter = iter.into_iter();
-
-        for ele in &mut
-        // VERY VERY IMP. If you change this, then also change the impl Iterator for IterableBaseCompColBases
-        // so the corresponding bases are collected into their corresponding spaces
-            [&mut c.bases.A,
-             &mut c.bases.C,
-             &mut c.bases.G,
-             &mut c.bases.T, 
-             &mut c.bases.N].iter_mut() {
-            **ele = match iter.next() {
-                Some(n) => n,
-                None => panic!("Improper iterator recieved"),
-            }
-        }
-
-        c.bases
-    }
-}
-
-#[cfg(test)]
-mod col_base_comp_tests {
-    use super::*;
-
-    #[test]
-    fn test_iterability() {
-        // test if conversion TO iterator works
-        let mut read = BaseCompCol::new(0);
-        read.extract(&b'A');
-
-        let mut iter = read.bases.iter();
-        assert_eq!(iter.next().unwrap(), 1);
-        assert_eq!(iter.next().unwrap(), 0);
-
-        // test if converstion FROM iterator works
-        let mut read = BaseCompCol::new(0);
-        read.extract(&b'A');
-
-        let iter = read.bases.iter();
-        let converted_read: BaseCompColBases = iter.collect();
-
-        assert_eq!(converted_read, BaseCompColBases {A: 1, T: 0, G: 0, C: 0, N: 0});
-    }
-
-    #[test]
-    fn test_extract() {
-        let mut read = BaseCompCol::new(0);
-        read.extract(&b'A');
-        assert_eq!(read.bases.A, 1);
-
-        read.extract(&b'C');
-        assert_eq!(read.bases.C, 1);
-
-        read.extract(&b'T');
-        assert_eq!(read.bases.T, 1);
-
-        read.extract(&b'G');
-        assert_eq!(read.bases.G, 1);
-
-        read.extract(&b'N');
-        assert_eq!(read.bases.N, 1);
-    }
-    #[test]
-    fn test_percentage() {
-        let mut read = BaseCompCol::new(0);
-        for c in "ACTGN".as_bytes().iter() {
-            read.extract(c);
-        }
-        read.bases.percentage();
-
-        println!("{:?}", read);
-
-        assert_eq!(read.bases.A, 20, "Testing A");
-        assert_eq!(read.bases.C, 20, "Testing C");
-        assert_eq!(read.bases.T, 20, "Testing T");
-        assert_eq!(read.bases.G, 20, "Testing G");
-        assert_eq!(read.bases.N, 20, "Testing N");
-    }
-}
-
-impl BaseCompCol {
-    pub fn new (pos: usize) -> BaseCompCol {
-        BaseCompCol {pos, bases: BaseCompColBases {A: 0, T: 0, G: 0, C: 0, N: 0}}
-    }
-
-    pub fn extract (&mut self, s: &u8) {
-        match s {
-            b'A' => self.bases.A += 1,
-            b'T' => self.bases.T += 1,
-            b'G' => self.bases.G += 1,
-            b'C' => self.bases.C += 1,
-            b'N' => self.bases.N += 1,
-            _ => panic!("Invalid character {:?} == {:?} found in read", *s as char, s.to_ascii_lowercase())
-        }            
-    }
-
-    
-}
-
-/// Represents the entire base composition.
-/// As a Vec of Reads, each of which hold data for a single column
-#[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct BaseComp {
-    pub lib: Vec<BaseCompCol>,
-    len: usize,
-}
-
-impl BaseComp {
-    pub fn init (len: usize) -> BaseComp {
-        let mut base_comp = BaseComp { lib: Vec::with_capacity(len), len};
-        for i in 1..=len {
-            base_comp.lib.push(BaseCompCol::new(i));
-        }
-
-        base_comp
-    }
-
-    pub fn len (&self) -> usize {
-        self.lib.len()
-    }
-
-    pub fn extract (&mut self, s: &str) {
-        for c in s.as_bytes().iter().enumerate() {
-            self.lib[c.0].extract(c.1);
-        }
-    }
-
-    pub fn jsonify (&mut self) -> String {
-        self.len = BaseComp::len(&self);
-        serde_json::to_string(&self).expect("Error encountered JSONifying data")
     }
 }
